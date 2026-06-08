de838bfec1d72d9b8f5b31bb7086df1b970613b7
Kauã Sousa <sousakaua2409@gmail.com>
2026-06-08 18:15:51 -0300

    Imagens e Links

--- js/linkar.js
+++ js/linkar.js
@@ -6,6 +6,34 @@ function abrirCatalogo(){
     window.location.href = "catalogo.html"
 }
 
+function abrirCategoriaPingente(){
+    window.location.href = "categoriaPingente.html"
+}
+
+function abrirCategoriaColarMesa(){
+    window.location.href = "categoriaColarMesa.html"
+}
+
+function abrirCategoriaColarParede(){
+    window.location.href = "categoriaColarParede.html"
+}
+
+function abrirCategoriaAbracadeira(){
+    window.location.href = "categoriaAbracadeira.html"
+}
+
+function abrirInsta(){
+    window.location.href = "https://www.instagram.com/zambi_decora?igsh=cTZxeHQzZDAycHFj"
+}
+
+function abrirPin(){
+    window.location.href = "https://pin.it/c2vhtIlTq"
+}
+
+function abrirFace(){
+    window.location.href = "https://www.facebook.com/share/1MWtciEfRt/"
+}
+
 function voltarPagina(){
     if(history.length > 1){
         history.back();
